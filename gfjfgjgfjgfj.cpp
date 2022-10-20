48629782402a228c83488bf0e1472940552abb9d
Ghoul12342 <114170053+Ghoul12342@users.noreply.github.com>
2022-10-20 18:44:09 +0300

    Add files via upload

---
+++ gfjfgjgfjgfj.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+using namespace std;
+int main() {
+	//18. «адано натуральное число N.Ќайти количество натуральных чисел, не превосход€щих N и не дел€щихс€ ни на одно из чисел 2, 3, 5.
+	/*int n;
+	cin >> n;
+	int i=1;
+	int k = 0;
+	while (i <= n) {
+		if (i % 2 != 0 && i % 3 != 0 && i % 5 != 0) {
+			k++;
+		}
+		i++;
+	}
+	cout << k<<" ";*/
+	//1
+	/*int i = 7;
+	while (i <= 98) {
+		cout << i << " ";
+		i += 7;
+	}*/
+	
+	//2
+/*	int n;
+	cin >> n;
+	int i= 1;
+	while (i <= n) {
+		cout <<"$";
+		i++;
+	}*/
+//3
+	/*int n = 0;
+	int i = 20;
+	while (i <= 78) {
+		if (i % 2 == 0) {
+			n = n + i;
+			
+
+		}
+		i++;
+
+	}
+	cout << n << " ";
+	//4
+	/*int a, b;
+	cin >> a >> b;
+	int i = a;
+	int z=1;
+	while (i <= b) {
+		if (i % 9 == 2 || i%9 == 3) {
+			z = z * i;
+			
+			
+		}
+		i++;
+	}
+	
+	cout << z << " ";*/
+	//5
+	/*int a, b;
+	cin >> a >> b;
+	int i = b;
+	
+	while (i >= a) {
+		if (i % 7 == 0) {
+			cout << i << " ";
+			
+
+		}
+
+		i--;
+	}*/
+	
+
+
+
+
+
+
+
+
+
+
+
+}
